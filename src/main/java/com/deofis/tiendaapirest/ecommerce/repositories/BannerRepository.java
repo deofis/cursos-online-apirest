1a3afa4bc6f5623c68ed068a2977b64a9dceafc9
neritoo <ezegavilan95@gmail.com>
2021-02-05 13:48:59 -0300

    Refactorizado BannerService para asignación de ordenes.

--- src/main/java/com/deofis/tiendaapirest/ecommerce/repositories/BannerRepository.java
+++ src/main/java/com/deofis/tiendaapirest/ecommerce/repositories/BannerRepository.java
@@ -2,11 +2,15 @@ package com.deofis.tiendaapirest.ecommerce.repositories;
 
 import com.deofis.tiendaapirest.ecommerce.domain.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Long> {
 
     boolean existsByOrden(Integer orden);
+
+    @Query("SELECT MAX(b.orden) as Orden FROM Banner b")
+    Integer ultimoOrden();
 
 }
